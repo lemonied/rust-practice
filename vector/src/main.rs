7da8ad6340ac0c687dc6ae2f9e5415f75ad9d749
ChenJiYuan <447334358@qq.com>
2025-12-29 10:41:37 +0000

    feat: development

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -3,5 +3,10 @@ fn main() {
   for i in &mut v {
     *i += 1;
   }
+  let v = vec![1, 2, 3];
+  let a: Vec<_> = v.iter().filter(|i| *i % 2 != 0).collect();
+  let b: Vec<_> = v.iter().map(|i| i + 1).filter(|i| i % 2 != 0).collect();
+  println!("a: {:?}", a);
+  println!("b: {:?}", b);
   println!("{:#?}", v);
 }
